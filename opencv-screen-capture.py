6d5d881548e125655555a0ae2bd004c451a12876
Edmund Bu <edmundpenguin@gmail.com>
2024-08-18 14:01:27 -0700

    Renamed deep cnn file, import model

--- opencv-screen-capture.py
+++ opencv-screen-capture.py
@@ -1,2 +1,10 @@
 import cv2 
 import numpy as np
+import torch
+import torch.nn as nn
+from train_torch.nmist_train_torch_CNN_deep import EnhancedCNN
+
+model = EnhancedCNN()
+model.load_state_dict(torch.load('cnn-deep-model.pth'))
+
+
