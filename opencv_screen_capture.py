ce05abed00a981535abb9bc3f37138c3fbdf77d5
Edmund Bu <edmundpenguin@gmail.com>
2024-08-22 14:18:54 -0700

    Commit

--- opencv_screen_capture.py
+++ opencv_screen_capture.py
@@ -1,10 +1,17 @@
 import cv2 
+import pygetwindow as gw
+from PIL import ImageGrab
+
 import numpy as np
 import torch
 import torch.nn as nn
 from train_torch.nmist_train_torch_CNN_deep import EnhancedCNN
-import pygetwindow
 
+import threading
+from flask import Flask, render_template
+
+# Load model ----------------------------------------------------------------------
 model = EnhancedCNN()
 model.load_state_dict(torch.load('models/cnn-deep-model.pth', weights_only=True))
+model.eval()
 
